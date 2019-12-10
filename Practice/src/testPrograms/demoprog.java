b2074e65419119c27409d8622bb2cdca179af6b1
shaik.aslam <shaik.aslam@izmoltd.com>
2019-12-10 12:27:10 +0530

    commit

---
+++ Practice/src/testPrograms/demoprog.java
@@ -0,0 +1,18 @@
+package testPrograms;
+
+public class demoprog {
+	int x=10;
+	static int y=20;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		demoprog t1= new demoprog();
+		t1.x=11;
+		t1.y=22;
+		demoprog t2= new demoprog();
+		System.out.println(t1.x+ t1.y);
+		
+
+	}
+
+}
